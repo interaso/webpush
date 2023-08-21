59851fc78a848abf44834edca7c5eba46f96675a
Lukáš Moravec <morki@morki.cz>
2023-08-22 01:32:57 +0200

    Add Nexus publishing and Git versioning setup to the build script
    Updated build.gradle.kts and libs.versions.toml to incorporate git-versioning and nexus-publish plugins for better version management and ease of publishing to Maven Central. Adjusted the project versioning to follow Git tags and added the nexus publishing setup for easier deployment of project artifacts. Additionally, refined the setup for the javadocJar task and signing configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,33 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.nexus.publish)
+    alias(libs.plugins.git.versioning)
     `java-library`
     `maven-publish`
+    signing
 }
 
 group = "com.interaso"
-version = "0.0.1"
+version = "0.0.0-SNAPSHOT"
+
+gitVersioning.apply {
+    describeTagPattern = "v(?<version>[0-9]+\\.[0-9]+\\.[0-9]+)"
+    refs {
+        tag(describeTagPattern) {
+            version = "\${ref.version}"
+        }
+        branch(describeTagPattern.removePrefix("v")) {
+            version = "\${ref.version}-SNAPSHOT"
+        }
+    }
+    rev {
+        version = "\${describe.tag.version.major}.\${describe.tag.version.minor.next}.0-SNAPSHOT"
+    }
+}
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    dokkaHtmlPlugin(libs.dokka.versioning.plugin)
-}
-
-java {
-    withSourcesJar()
-    withJavadocJar()
 }
 
 kotlin {
@@ -26,16 +35,63 @@ kotlin {
     explicitApi()
 }
 
-tasks {
-    named<Jar>("javadocJar") {
-        from(dokkaJavadoc)
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.named<Jar>("javadocJar") {
+    from(tasks.dokkaJavadoc)
+}
+
+signing {
+    isRequired = false
+    sign(publishing.publications)
+    useInMemoryPgpKeys(
+        providers.gradleProperty("gpg.key").orNull,
+        providers.gradleProperty("gpg.password").orNull,
+    )
+}
+
+nexusPublishing.repositories {
+    sonatype {
+        nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
+        snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+
+        username = providers.gradleProperty("sonatype.username").orNull
+        password = providers.gradleProperty("sonatype.password").orNull
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            from(components["java"])
+publishing.publications {
+    create<MavenPublication>("release") {
+        from(components["java"])
+        pom {
+            name = "WebPush"
+            description = "Lightweight library for sending web push notifications with zero dependencies."
+            url = "https://github.com/interaso/webpush"
+            licenses {
+                license {
+                    name = "The MIT License (MIT)"
+                    url = "https://opensource.org/licenses/MIT"
+                }
+            }
+            organization {
+                name = "Interactive Solutions s.r.o."
+                url = "https://interaso.com"
+            }
+            developers {
+                developer {
+                    id = "morki"
+                    name = "Lukáš Moravec"
+                    email = "morki@morki.cz"
+                }
+            }
+            scm {
+                connection = "scm:git:https://github.com/interaso/webpush.git"
+                developerConnection = "scm:git:ssh://git@github.com/interaso/webpush.git"
+                url = "https://github.com/interaso/webpush"
+            }
         }
     }
 }
