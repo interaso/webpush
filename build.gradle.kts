a168147c8941cf6db1a54a2254ea95d130ad3a64
morki <morki@morki.cz>
2023-08-22 11:31:19 +0200

    Refactor build.gradle.kts for cleaner, more concise syntax
    This commit cleans up the syntax in the build.gradle.kts file by removing unnecessary nesting of various blocks (e.g., gitVersioning, nexusPublishing, publishing.publications). Also, nexusPublishing.transitionCheckOptions block added to implement retries during transitions that fail, improving build stability.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.time.*
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.dokka)
@@ -13,13 +15,11 @@ version = "0.0.0-SNAPSHOT"
 
 gitVersioning.apply {
     describeTagPattern = "v(?<version>[0-9]+\\.[0-9]+\\.[0-9]+)"
-    refs {
-        tag(describeTagPattern) {
-            version = "\${ref.version}"
-        }
-        branch(describeTagPattern.removePrefix("v")) {
-            version = "\${ref.version}-SNAPSHOT"
-        }
+    refs.tag(describeTagPattern) {
+        version = "\${ref.version}"
+    }
+    refs.branch(describeTagPattern.removePrefix("v")) {
+        version = "\${ref.version}-SNAPSHOT"
     }
     rev {
         version = "\${describe.tag.version.major}.\${describe.tag.version.minor.next}.0-SNAPSHOT"
@@ -53,45 +53,46 @@ signing {
     )
 }
 
-nexusPublishing.repositories {
-    sonatype {
-        nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
-        snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+nexusPublishing.repositories.sonatype {
+    nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
+    snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 
-        username = providers.gradleProperty("sonatypeUsername").orNull
-        password = providers.gradleProperty("sonatypePassword").orNull
-    }
+    username = providers.gradleProperty("sonatypeUsername").orNull
+    password = providers.gradleProperty("sonatypePassword").orNull
 }
 
-publishing.publications {
-    create<MavenPublication>("release") {
-        from(components["java"])
-        pom {
-            name = "WebPush"
-            description = "Lightweight library for sending web push notifications with zero dependencies."
+nexusPublishing.transitionCheckOptions {
+    maxRetries = 10
+    delayBetween = Duration.ofSeconds(5)
+}
+
+publishing.publications.create<MavenPublication>("release") {
+    from(components["java"])
+    pom {
+        name = "WebPush"
+        description = "Lightweight library for sending web push notifications with zero dependencies."
+        url = "https://github.com/interaso/webpush"
+        licenses {
+            license {
+                name = "The MIT License (MIT)"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+        organization {
+            name = "Interactive Solutions s.r.o."
+            url = "https://interaso.com"
+        }
+        developers {
+            developer {
+                id = "morki"
+                name = "Lukáš Moravec"
+                email = "morki@morki.cz"
+            }
+        }
+        scm {
+            connection = "scm:git:https://github.com/interaso/webpush.git"
+            developerConnection = "scm:git:ssh://git@github.com/interaso/webpush.git"
             url = "https://github.com/interaso/webpush"
-            licenses {
-                license {
-                    name = "The MIT License (MIT)"
-                    url = "https://opensource.org/licenses/MIT"
-                }
-            }
-            organization {
-                name = "Interactive Solutions s.r.o."
-                url = "https://interaso.com"
-            }
-            developers {
-                developer {
-                    id = "morki"
-                    name = "Lukáš Moravec"
-                    email = "morki@morki.cz"
-                }
-            }
-            scm {
-                connection = "scm:git:https://github.com/interaso/webpush.git"
-                developerConnection = "scm:git:ssh://git@github.com/interaso/webpush.git"
-                url = "https://github.com/interaso/webpush"
-            }
         }
     }
 }
